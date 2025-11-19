d5bbba65fb482725fad378238570cdf333a09465
IsaacKMHedman <hedmanisaac@gmail.com>
2025-11-19 15:06:43 +0100

    123

--- Assignment 3/Task 4/main.js
+++ Assignment 3/Task 4/main.js
@@ -27,6 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
     function circleFunction(){
         console.log('Circle Function');
         for(let j = 0; j < circles.length; j++){
+            //Toggle fungerar lika bra, eller? 
+            // Bör väl fungera lika bra som remove och add på classList...?
             circles[j].classList.toggle('green');
             console.log(circles[j].classList);
         }
@@ -34,18 +36,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function squareFunction(){
         console.log('Square Function');
+        let squareStyle = window.getComputedStyle(squares[0]);
+
+        //Halfa av den aktuella storleken
+        let halfWidth = parseFloat(squareStyle.width)/2
+        let halfHeight = parseFloat(squareStyle.height)/2;
         for(let j = 0; j < squares.length; j++){
-            squares[j].classList.toggle('green');
-            console.log(squares[j].classList);
-        }
-    }
-    
-    function rectangleFunction(){
-        console.log('Rectangle Function');
-        for(let j = 0; j < rectangles.length; j++){
-            rectangles[j].classList.toggle('green');
-            console.log(rectangles[j].classList);
+            squares[j].style.width = halfWidth + "px";
+            squares[j].style.height = halfHeight + "px";
         }
     }
 
+    function rectangleFunction(){
+        console.log('Rectangle Function');
+        let rectangleStyle = window.getComputedStyle(rectangles[0]);
+
+        //Current position
+        let currentPosition = parseFloat(rectangleStyle.top);
+        console.log(parseFloat(rectangleStyle.top));
+        for(let j = 0; j < rectangles.length; j++){
+            rectangles[j].style.top = (currentPosition + 10 + 'px');
+        }
+    }
 });
