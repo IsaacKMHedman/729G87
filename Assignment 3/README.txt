5649aa01b1bfe146df1eec5b386816c0d49bf8ff
IsaacKMHedman <hedmanisaac@gmail.com>
2025-11-21 13:29:12 +0100

    Allt klart

---
+++ Assignment 3/README.txt
@@ -0,0 +1,3 @@
+Optional tasks on excercises 2 & 3
+
+Aiming for 2 points. 
---
+++ Assignment 3/Task 5/main.js
@@ -0,0 +1,26 @@
+import {animate } from 'https://esm.sh/animejs';
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    //Skapar variabler, rectangleklassen som man ska iterera över.
+    // Sedan style på rectangle. 
+    // Sedan även den heighten
+    let rectangles = document.getElementsByClassName('rectangle');
+    let rectangleStyle = window.getComputedStyle(rectangles[0]);
+    let rectangleHeight = rectangleStyle.height;
+
+    //Alla rektanglar får eventlisteners för mouse enter och mouse leave
+    for(let i = 0; i < rectangles.length; i++){
+        rectangles[i].addEventListener("mouseenter", (event) => {
+            animate(event.target, { 
+                height: 500,
+            })
+        });
+
+        rectangles[i].addEventListener("mouseleave", (event) => {
+            animate(event.target, {
+                height: rectangleHeight,
+            })
+        });
+    }
+});
