1fc4c48e0a92e3e322d4692d64f9b7782d61d539
isahe131 <isahe131@student.liu.se>
2025-11-24 10:28:17 +0100

    123

--- Assignment 3/Task 1/main.js
+++ Assignment 3/Task 1/main.js
@@ -1,17 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    
     let bluecube = document.getElementById('bluecubebackground');
 
 
     bluecube.onmouseenter = () =>{
         //bluecube.style.display="none";
-        bluecube.classList.remove("leave");
         bluecube.classList.add("enter");
     };
-    bluecube.onmouseleave = () => {
-       // bluecube.style.display="block";  
-       bluecube.classList.remove("enter")
-       bluecube.classList.add("leave");
-    };
+
     
 
 });
